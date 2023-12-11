621850c33cb8097fab673910a4577b5427fcb190
Bruno Martins <bgcngm@gmail.com>
2023-12-11 16:04:59 +0000

    msm8996-common: configpanel: Re-style
    This brings user interface inline with Settings.
    
    Change-Id: I73f8a68a8bf99897af8a93030367af046fb5a5b7

--- configpanel/src/org/lineageos/settings/device/ButtonSettingsFragment.java
+++ configpanel/src/org/lineageos/settings/device/ButtonSettingsFragment.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- *           (C) 2017,2020-2021 The LineageOS Project
+ *           (C) 2017,2019-2023 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@ import android.app.ActionBar;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.MenuItem;
 
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
