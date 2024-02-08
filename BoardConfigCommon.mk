b7f214db2f3428bbb5453ea960306ada0011ac4c
Bruno Martins <bgcngm@gmail.com>
2024-02-08 17:05:32 +0000

    msm8996-common: Use the common device compatibility matrix from QCOM
    Change-Id: I0c0f3ecddbe6d7b232cc0d13c78ca58094e4e720

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2021 The LineageOS Project
+# Copyright (C) 2017-2024 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -108,7 +108,7 @@ DEVICE_SPECIFIC_GPS_PATH := $(VENDOR_PATH)/gps
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(VENDOR_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
