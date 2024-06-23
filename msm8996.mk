e43e57c3bd6c3f82addcaae4d377b71b41d028d8
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    msm8996-common: Move to new RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495

--- msm8996.mk
+++ msm8996.mk
@@ -23,6 +23,9 @@
 #
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/zuk/msm8996-common/msm8996-common-vendor.mk)
 
