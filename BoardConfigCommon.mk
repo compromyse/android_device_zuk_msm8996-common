819759b6b9b6a989b32df5ef4ffdbbbd0db83157
Raghuram Subramani <raghus2247@gmail.com>
2024-12-21 14:25:54 +0530

    msm8996-common: Update HALs in the manifest & re-add framework_compatibility_matrix.xml

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,7 +109,8 @@ DEVICE_SPECIFIC_GPS_PATH := $(VENDOR_PATH)/gps
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml \
+    $(VENDOR_PATH)/framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(VENDOR_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
